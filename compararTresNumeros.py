01871dc11360b4c24e1883a831c74e85fee01828
AndresGonzaloBeltranAlmeyda <160796511+AndresGonzaloBealtranAlmeyda@users.noreply.github.com>
2024-02-23 09:24:01 -0500

    ejerciciosPython

---
+++ compararTresNumeros.py
@@ -0,0 +1,15 @@
+n1 = int(input("Ingrese numero1: "))
+n2 = int(input("Ingrese numero2: "))
+n3 = int(input("Ingrese numero3: "))
+
+if n1 > n2 and n1 > n3:
+    print("El numero ", n1, " es el mayor")
+
+elif n2 > n1 and n2 > n3:
+    print("El numero ", n2, " es el mayor")
+
+elif n3 > n2 and n3 > n1:
+    print("El numero ", n3, " es el mayor")
+
+else:
+    print("Los 3 numeros son iguales")
---
+++ sumarDosNumeros.py
@@ -0,0 +1,3 @@
+n1 = int(input("Ingrese el numero1: "))
+n2 = int(input("Ingrese el numero2: "))
+print(n1 + n2)
